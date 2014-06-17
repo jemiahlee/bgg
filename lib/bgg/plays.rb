dd82c4f1f08a3a573a0f37b15f1b0abe910f6d2c
Kevin Craine <kcraine@silvercloudinc.com>
2014-06-17 00:00:42 -0400

    Add plays result container

--- lib/bgg/plays.rb
+++ lib/bgg/plays.rb
@@ -1,19 +1,65 @@
 module Bgg
-	class Plays
-		def self.find_by_username(username)
-			if username =~ /^\d*$/
-				# this is a sorta hokey restriction because there is probably nothing requiring
-				# users to have non-numeric usernames. I'm not sure that I've seen one though.
-				raise ArgumentError.new('username must not be empty or only digits!')
-			end
+  class Plays
+    include Bgg::Result::Container
+    attr_reader :page, :thing_id, :total_count, :username
 
-			if username.is_a?(Integer)
-				raise ArgumentError.new('username must not be an Integer!')
-			end
+    def initialize(item, request)
+      super item, request
 
-			iterator = Bgg::Plays::Iterator.new(username)
+      @page         = request_params[:page]
+      @thing_id     = request_params[:id]
+      @total_count  = xpath_value_int "plays/@total"
+      @username     = request_params[:username]
+    end
 
-			iterator.nil? ? nil : iterator
-		end
-	end
+    def find_by_date(date)
+      if date.kind_of? Range
+        select { |play| date.cover? play.date }
+      else
+        select { |play| play.date === date }
+      end
+    end
+
+    def find_by_location(location)
+      select { |play| play.location == location }
+    end
+
+    def find_by_thing_id(id)
+      select { |play| play.id == id }
+    end
+
+    def find_by_thing_name(name)
+      select { |play| play.name == name }
+    end
+
+    def board_game_expansions
+      find_by_type 'boardgameexpansion'
+    end
+
+    def board_game_implementations
+      find_by_type 'boardgameimplementation'
+    end
+
+    def board_games
+      find_by_type 'boardgame'
+    end
+
+    def rpg_items
+      find_by_type 'rpgitem'
+    end
+
+    def video_games
+      find_by_type 'videogame'
+    end
+
+    private
+
+    def parse
+      super 'plays/play', self.class::Play
+    end
+
+    def find_by_type type
+      select{ |play| play.types and play.types.include? type }
+    end
+  end
 end
