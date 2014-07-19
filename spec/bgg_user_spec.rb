4a616063ccfdab7ca40414f2f49028f4654f6698
Kevin Craine <kcraine@silvercloudinc.com>
2014-07-18 21:41:06 -0400

    Getting user object to work with new play objects

--- spec/bgg_user_spec.rb
+++ spec/bgg_user_spec.rb
@@ -73,7 +73,7 @@ describe Bgg::User do
       describe '.play_count' do
         it 'returns the number of plays when the user has plays' do
           stub_request(:any, 'http://www.boardgamegeek.com/xmlapi2/plays').
-            with(query: {username: 'texasjdl', page: 1}).
+            with(query: {username: 'texasjdl'}).
             to_return(body: File.open('sample_data/plays?username=texasjdl&page=1'), status: 200)
 
           expect( texasjdl.play_count ).to eq(299)
@@ -95,18 +95,16 @@ describe Bgg::User do
 
       describe '.plays' do
         it 'returns a Bgg::PlaysIterator' do
-          [1,2,3].each do |i|
-            stub_request(:any, 'http://www.boardgamegeek.com/xmlapi2/plays').
-              with(query: {username: 'texasjdl', page: i}).
-              to_return(body: File.open("sample_data/plays?username=texasjdl&page=#{i}"), status: 200)
-          end
+          stub_request(:any, 'http://www.boardgamegeek.com/xmlapi2/plays').
+            with(query: {username: 'texasjdl'}).
+            to_return(body: File.open("sample_data/plays?username=texasjdl&page=1"), status: 200)
 
           plays = texasjdl.plays
           first_play = plays.first
 
-          expect( plays ).to be_an_instance_of(Bgg::Plays::Iterator)
-          expect( first_play ).to be_an_instance_of(Bgg::Play)
-          expect( first_play.game_name ).to eq('Fauna')
+          expect( plays ).to be_instance_of(Bgg::Result::Plays)
+          expect( first_play ).to be_instance_of(Bgg::Result::Plays::Play)
+          expect( first_play.name ).to eq('Fauna')
           expect( first_play.players.size ).to eq(5)
           expect( first_play.players.first.name ).to eq('Ted')
         end
